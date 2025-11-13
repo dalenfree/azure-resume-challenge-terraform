6bc4c5d9253ec952a90ec4e3e1a3d0fe4a9a1af4
dalenfree <dalen.mcclintock@gmail.com>
2025-11-12 21:00:26 -0500

    updated app.js to point to correct endpoint

--- frontend/app.js
+++ frontend/app.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const incrementVisitorCount = async () => {
         try {
             // POST request to the API
-            const postResponse = await fetch('https://func-t4lmqhxdjmwxw.azurewebsites.net/api/http_trigger', {
+            const postResponse = await fetch('https://fa96407.azurewebsites.net/api/http_trigger', {
             //const postResponse = await fetch('http://localhost:7071/api/http_trigger', {
                 method: 'POST',
                 headers: {
